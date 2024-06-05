c3f34b2e57603020d6d35ea5862cf4a7e2a83b14
digitsmyanmar72 <naywinaung522522@gmail.com>
2024-06-05 15:32:38 +0630

    second

--- wp-content/plugins/envo-extra/lib/elementor/assets/js/elementor.js
+++
@@ -1,8 +0,0 @@
-(function ($) {
-    'use strict';
-
-    $(document).ready(function(){   
-      $('.e-notice-bar__action a').attr('href','').attr('href','https://be.elementor.com/visit/?bta=1876&brand=elementor');
-    });
-
-})(jQuery);
